06a4b871e5598483c7ec03ab5662bc7d6cb4c950
Neil <zguolee@foxmail.com>
2022-04-12 15:31:03 +0800

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import Unocss from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import UnocssToUni from 'vite-plugin-unocss-to-uni'
+import { UnocssToUni } from 'vite-plugin-unocss-to-uni'
 
 // https://vitejs.dev/config/
 export default defineConfig({
